4f2496f9e2797f0c83b50d6afed25cbc270b3f23
Giuditta <giudittamarino89@gmail.com>
2021-03-01 16:24:32 +0100

    esercizio terminato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,32 @@
-var numeroChilometri = prompt("Quanti km vuoi percorrere");
+// CHIEDO NUMERO KM E SALVO IN VARIABILE
+var numeroChilometri = parseFloat(prompt("Quanti km vuoi percorrere? (Inserisci un valore in cifre)"));
 
-var etaPasseggero = prompt("Quanti anni hai?");
+// CHIEDO ETA PASSEGGERO E SALVO IN VARIABILE
+var etaPasseggero = parseInt(prompt("Quanti anni hai? (Inserisci un valore in cifre)"));
 
 console.log(numeroChilometri, etaPasseggero);
+
+// CALCOLO PREZZO BASE E SALVO IN VARIABILE
+var prezzoBase = 0.21 * numeroChilometri;
+
+console.log(prezzoBase);
+
+// INSERISCO VARIABILI PER SCONTI DEL 20 E 40
+var sconto20 = prezzoBase * 20 / 100;
+
+var sconto40 = prezzoBase * 40 / 100;
+
+// RESTITUISCO DEGLI ALERT SE I DATI INSERITI NON SONO VALORI  NUMERICI O SE L'ETA' NON E' UN NUMERO REALISTICO, ALTRIMENTI CALCOLO IL PREZZO FINALE DEL BIGLIETTO E LO STAMPO  NEL DOM NELL'ELEMENTO CON ID PREZZOFINALE
+if (isNaN(numeroChilometri) || isNaN(etaPasseggero)){
+  alert("I dati che hai inserito non sono corretti. Ricordati di inserire dati numerici.");
+} else if (etaPasseggero < 0 || etaPasseggero > 120){
+  alert("L'età che hai inserito non è valida. Inserisci valori compresi tra 0 e 120");
+} else {
+    if (etaPasseggero < 18) {
+      document.getElementById("prezzofinale").innerHTML= parseFloat(prezzoBase - sconto20).toFixed(2) + " euro (scontato del 20%).";
+    } else if (etaPasseggero > 65) {
+        document.getElementById("prezzofinale").innerHTML= parseFloat(prezzoBase - sconto40).toFixed(2) + " euro (scontato del 40%).";
+    } else {
+      document.getElementById("prezzofinale").innerHTML= parseFloat(prezzoBase).toFixed(2) + " euro.";
+    }
+  }
